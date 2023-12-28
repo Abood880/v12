a1b1bb8a12dbec3068d6a9ab1f3145470804f785
Abood880 <125342457+Abood880@users.noreply.github.com>
2023-12-28 16:29:15 +0300

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
-session1 = os.environ.get("TERMUX")
-SESSION1 = os.environ.get("TERMUX")
+APP_ID = os.environ.get("15078775")
+APP_HASH = os.environ.get("0f98bdfe43717ab2f67dbfcea416131c")
+session1 = os.environ.get("1AZWarzwBu4u9tfTKw6NdZGofS__mGaNY6lmGYIaOOSq3cV4h6oGTS0ghuwgGSILD01A8Ma-4wfXjppwf3w7G8PQUQhnNl7samlsSaBEZeVtO73QFcHcyv6KDsLusZJFTgJcfNion_mjKVAjUV4cQ5ntPLurD37loMm2ZnaTsVTxswc6zAXIO122pZiiTvDg4uW7leUNUangzT4HUJBe0t6ITenIjepB1yZLe0iP79HKZlHsEXnH8r2pk2SPpyt6sp-FS7aFOmQkPTggq6NiS3z-IziJRVyOBV3osf9xHXs-cUczj5DFS_L6yoKDd-eUF_YFiaFczZMQWFhuvypp3R3OwZzK3_cU=")
+SESSION1 = os.environ.get("1AZWarzwBu4u9tfTKw6NdZGofS__mGaNY6lmGYIaOOSq3cV4h6oGTS0ghuwgGSILD01A8Ma-4wfXjppwf3w7G8PQUQhnNl7samlsSaBEZeVtO73QFcHcyv6KDsLusZJFTgJcfNion_mjKVAjUV4cQ5ntPLurD37loMm2ZnaTsVTxswc6zAXIO122pZiiTvDg4uW7leUNUangzT4HUJBe0t6ITenIjepB1yZLe0iP79HKZlHsEXnH8r2pk2SPpyt6sp-FS7aFOmQkPTggq6NiS3z-IziJRVyOBV3osf9xHXs-cUczj5DFS_L6yoKDd-eUF_YFiaFczZMQWFhuvypp3R3OwZzK3_cU=")
 DEVLOO = os.environ.get("DEVLO")
 CHNA = os.environ.get("CHNA")
-TG_BOT_USERNAME = os.environ.get("TG_BOT_USERNAME")
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+TG_BOT_USERNAME = os.environ.get("Ysjsewbot")
+TG_BOT_TOKEN = os.environ.get("6545664999:AAFrU0aB6f77MCyF1M-nOfshwcYAxNdNDU4")
 abbas = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
 ispay = ['yes']
 ispay2 = ['yes']
